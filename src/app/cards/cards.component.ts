fd4c1a33fd8ace13b2ee8ec934424f559b2bc8b2
Sam Smith <korhan.tokgoz92@gmail.com>
2022-04-19 03:14:29 +0300

    first commit

---
+++ src/app/cards/cards.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {CardModalComponent} from './card-modal/card-modal.component';
+import {CardService} from '../services/card.service';
+import { Card } from '../models/card';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.scss']
+})
+export class CardsComponent implements OnInit {
+
+  cards!: Card[];
+
+  
+
+  constructor(
+    public dialog: MatDialog,
+    public cardService: CardService
+  ) { }
+
+  ngOnInit(): void {
+    this.getCards();
+  }
+openAddCardModal(): void{
+  this.dialog.open(CardModalComponent, {
+    width:'400px'
+  });
+}
+getCards():void {
+((res: Card[])=>{
+  console.log(res);
+  this.cards =res;
+
+});
+}
+}
+
---
+++ src/app/models/card.ts
@@ -0,0 +1,9 @@
+export interface Card {
+    name: string;
+    title:string;
+    birthDate: string;
+    gmail:string;
+    phone:string;
+    adress:string;
+
+}
